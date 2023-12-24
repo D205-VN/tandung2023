11ac1aba1a81926be39cb70994ffe0ea5ec1c940
D205-VN <76621504+D205-VN@users.noreply.github.com>
2023-12-24 19:22:45 +0700

    Update Mo.js

--- Mo.js
+++ Mo.js
@@ -3,6 +3,6 @@ document.addEventListener('keydown', function(event) {
     // Kiểm tra xem người dùng đã nhấn phím "R" và không nhấn phím Shift, Ctrl, hoặc Alt cùng lúc
     if (event.key === 'r' && !event.shiftKey && !event.ctrlKey && !event.altKey) {
       // Chuyển về trang đầu
-      window.location.href = 'tille.html';
+      window.location.href = 'index.html';
     }
   });
